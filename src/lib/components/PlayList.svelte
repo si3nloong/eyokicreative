8171c4f0581946d9cf4380cc735faf1c116a90d8
si3nloong <sianloong90@gmail.com>
2022-10-19 12:12:30 +0800

    chore: draft to fix media player

--- src/lib/components/PlayList.svelte
+++ src/lib/components/PlayList.svelte
@@ -20,10 +20,9 @@
 <script lang="ts">
 	import { mediaPlayer } from '$lib/store';
 	import YouTubePlayer from 'yt-player';
-	import { fade } from 'svelte/transition';
 
 	import Image from './Image.svelte';
-	import TagList from './TagList.svelte';
+	import MediaPlayer from './MediaPlayer.svelte';
 
 	export let items: Item[] = [];
 
@@ -35,7 +34,7 @@
 		selectedItem = item;
 		showDialog = true;
 		const { scrollY } = window;
-		document.body.setAttribute('style', `position: fixed; margin-top: -${scrollY}px`);
+		document.body.setAttribute('style', `position: fixed;`);
 		document.body.setAttribute('data-scrolly', `${scrollY}`);
 	};
 
@@ -87,70 +86,10 @@
 </ul>
 
 {#if showDialog && selectedItem}
-	<div class="modal">
-		<div class="overlay" in:fade out:fade on:click|stopPropagation={closeDialog} />
-		<div class="dialog">
-			<span class="close-btn" on:click={closeDialog}
-				>{@html `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M18 6L6 18" stroke="#33363F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-				<path d="M6 6L18 18" stroke="#33363F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`}</span
-			>
-			<div class="aspect-ratio-box">
-				<div class="cover">
-					<div class="video-player" bind:this={videoPLayer} />
-					{#if !playing}
-						<img class="cover-img" src={selectedItem.cover} alt={selectedItem.title} />
-					{/if}
-				</div>
-				{#if !playing}
-					<footer>
-						<button class="play-btn" on:click={playVideo}>
-							<span>Play</span>
-							{@html `<svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M16.1378 10.5689L9.60498 7.30252C8.40816 6.70411 7 7.5744 7 8.91249V15.0876C7 16.4257 8.40816 17.2959 9.60498 16.6975L16.1378 13.4311C17.3171 12.8415 17.3171 11.1586 16.1378 10.5689Z" fill="#fff"/></svg>`}
-						</button>
-					</footer>
-				{/if}
-			</div>
-			<div class="content">
-				<section class="main">
-					<h2>{selectedItem.title}</h2>
-					<!-- <div>Client</div> -->
-					<div>{selectedItem.time}</div>
-					<div>{@html selectedItem.shortDesc}</div>
-				</section>
-				<section class="info">
-					<!-- <div>Genre</div> -->
-					<div>
-						<span class="label">Director:</span>
-						<span><TagList items={selectedItem.directors} /></span>
-					</div>
-					{#if selectedItem.writers}
-						<div>
-							<span class="label">Writer:</span>
-							<span><TagList items={selectedItem.writers} /></span>
-						</div>
-					{/if}
-					{#if selectedItem.dps}
-						<div>
-							<span class="label">Director of Photography:</span>
-							<span><TagList items={selectedItem.dps} /></span>
-						</div>
-					{/if}
-					{#if selectedItem.editors}
-						<div><span class="label">Editor:</span>{selectedItem.editors.join(', ')}</div>
-					{/if}
-					<div><span class="label">Cast:</span>{selectedItem.casts.join(', ')}</div>
-					<div><span class="label">Produced By:</span> Eyoki Creative</div>
-				</section>
-			</div>
-		</div>
-	</div>
+	<MediaPlayer video={selectedItem} />
 {/if}
 
 <style lang="scss">
-	$paddingH: 2rem;
-
 	ul {
 		color: #424653;
 		list-style: none;
@@ -185,7 +124,7 @@
 				text-align: center;
 				margin: 0 0.25rem;
 
-				& > svg {
+				:global(svg) {
 					width: 100%;
 					height: 100%;
 					display: inline-flex;
@@ -197,130 +136,5 @@
 		li:last-child {
 			border: none;
 		}
-	}
-
-	.modal {
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		width: 100%;
-		height: 100%;
-		z-index: 10;
-
-		.overlay {
-			position: fixed;
-			top: 0;
-			left: 0;
-			right: 0;
-			bottom: 0;
-			width: 100%;
-			overflow-y: auto;
-			min-height: 100%;
-			background: rgba(0, 0, 0, 0.3);
-		}
-
-		.dialog {
-			position: relative;
-			// display: flex;
-			top: 2rem;
-			background: #fff;
-			width: 80%;
-			min-height: 100px;
-			margin: 0 auto;
-			max-width: 1280px;
-			overflow: hidden;
-			border-radius: 5px;
-			box-shadow: 0 0 26px rgba(0, 0, 0, 0.3);
-
-			.close-btn {
-				position: absolute;
-				cursor: pointer;
-				top: 1rem;
-				right: 1rem;
-				border-radius: 50%;
-				width: 34px;
-				height: 34px;
-				justify-content: center;
-				align-items: center;
-				display: inline-flex;
-				transition: background-color 0.5s;
-				background-color: #dcdcdc;
-				z-index: 1;
-
-				:global(svg) {
-					width: calc(100% - 8px);
-					height: calc(100% - 8px);
-				}
-
-				// &:hover {
-				// 	background-color: #dcdcdc;
-				// }
-			}
-
-			.content {
-				display: flex;
-				padding-bottom: 100px;
-				width: 100%;
-
-				.main {
-					flex-grow: 1;
-					padding: 1rem $paddingH;
-				}
-
-				.info {
-					min-width: 280px;
-					padding: 1rem $paddingH;
-					padding-left: 0;
-				}
-			}
-
-			footer {
-				position: absolute;
-				bottom: 5%;
-				left: $paddingH;
-			}
-			.play-btn {
-				display: flex;
-				align-items: center;
-				border: none;
-				min-height: 32px;
-				padding: 0 2rem 0 2.5rem;
-				border-radius: 3px;
-				background: red;
-				color: #fff;
-			}
-		}
-	}
-
-	.aspect-ratio-box {
-		position: relative;
-		display: block;
-		padding-top: 56.25%;
-		height: 0;
-		overflow: hidden;
-		background: #f5f5f5;
-
-		.cover,
-		.cover-img {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-		}
-	}
-
-	.video-player {
-		width: 100%;
-		height: 100%;
-		overflow: hidden;
-	}
-
-	.label {
-		font-size: 12px;
-		color: #3a3d42;
-		margin-right: 6px;
 	}
 </style>
