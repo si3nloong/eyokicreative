2f6a733fc0d5b6ad8bdad2255934370c073dff4e
si3nloong <sianloong90@gmail.com>
2022-11-27 00:58:51 +0800

    chore: add job position

--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -9,11 +9,47 @@
 <section>
 	<h1>Careers in Eyoki Creative</h1>
 	{#if opening}
-		<p>Internship</p>
+		<ul class="job-list">
+			<li>
+				<span class="position">Intern</span>
+				<a
+					class="apply-btn"
+					href="mailto:hr@eyokicreative.com?subject=Application for Intern position">Apply</a
+				>
+			</li>
+			<li>
+				<span class="position">Junior Director</span>
+				<a
+					class="apply-btn"
+					href="mailto:hr@eyokicreative.com?subject=Application for Junior Director position"
+					>Apply</a
+				>
+			</li>
+			<li>
+				<span class="position">Junior Director of Photography</span>
+				<a
+					class="apply-btn"
+					href="mailto:hr@eyokicreative.com?subject=Application for Junior DoP position">Apply</a
+				>
+			</li>
+		</ul>
 	{:else}
 		<div>Sorry, there are no opening at the moment.</div>
 	{/if}
 </section>
 
 <style lang="scss">
+	.job-list {
+		margin-top: 2rem;
+		list-style-position: inside;
+
+		li {
+			display: flex;
+			padding: 0.5rem 0;
+
+			.position {
+				flex-grow: 1;
+			}
+		}
+	}
 </style>
