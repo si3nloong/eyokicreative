f364684c12b7e6b22a41fc6a4c20b11a41de8f4a
si3nloong <sianloong90@gmail.com>
2022-12-11 18:41:14 +0800

    fix: meta tag

--- src/routes/all-works/+page.server.ts
+++ src/routes/all-works/+page.server.ts
@@ -5,15 +5,23 @@ import data from '$lib/data';
 import type { Media } from '$lib/components';
 
 export const load: PageServerLoad = ({ request }) => {
-	let video: Media | null = null;
 	const uri = new URL(request.url);
+	let video: Media | null = null;
 	if (uri.searchParams.has('id')) {
 		const id = uri.searchParams.get('id') || '';
 		video = data.find((v) => v.link === id) || null;
 	}
-	return {
+	let resp = {
 		producedBys: selfProduce.slice(),
 		dps: dp.slice(),
 		video
 	};
+	if (video) {
+		return Object.assign(resp, {
+			image: video.cover || '',
+			title: `EYOKI - ${video.title}`,
+			description: video.shortDesc || ''
+		});
+	}
+	return resp;
 };
